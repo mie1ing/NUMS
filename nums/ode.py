73871292fea8e04644d15bda83278fbc031845cd
Chengyuan Zhang <chengyuan.zhang@studium.uni-hamburg.de>
2025-04-24 16:28:38 +0200

    modify input of rk4

--- nums/ode.py
+++ nums/ode.py
@@ -1,13 +1,14 @@
 import numpy as np
 
-def rk4(f, y0, t):
+def rk4(f, y0, dt, n):
     """
     Solve ODE problem, i.e. dy/dt = f(y,t), using Runge−Kutta algorithm.
 
     Parameters:
     - f: function that receives current state y and time t, returns dy/dt
     - y0: initial state of the system, numpy array of shape (M,)
-    - t: array of time steps (length N)
+    - dt: time steps length
+    - n: number of time steps
 
     Returns:
     - y: (M x N) numpy array, each column is the state at corresponding time
@@ -15,23 +16,23 @@ def rk4(f, y0, t):
     Example: harmonic oscillator d²x/dt² = -x -> y = [dx/dt, x]
     f = lambda y, t: np.array([-y[1], y[0]])
     y0 = [0, 1]
-    t = np.linspace(0, 10, 1000)
+    dt = 0.05
+    n = 1000
 
     y = ode.rk4(f, y0, t)
     """
     y0 = np.array(y0)
     M = y0.shape[0]
-    N = len(t)
-    Y = np.zeros((M, N))
+    t = np.linspace(0, n*dt, n + 1)
+    Y = np.zeros((M, n + 1))
     Y[:, 0] = y0
-    delta = t[1] - t[0]
 
-    for n in range(N - 1):
+    for n in range(n):
         y1 = f(Y[:, n], t[n])
-        y2 = f(Y[:, n] + delta * y1 / 2, t[n] + delta / 2)
-        y3 = f(Y[:, n] + delta * y2 / 2, t[n] + delta / 2)
-        y4 = f(Y[:, n] + delta * y3, t[n] + delta)
-        Y[:, n + 1] = Y[:, n] + delta * (y1 + 2*y2 + 2*y3 + y4) / 6
+        y2 = f(Y[:, n] + dt * y1 / 2, t[n] + dt / 2)
+        y3 = f(Y[:, n] + dt * y2 / 2, t[n] + dt / 2)
+        y4 = f(Y[:, n] + dt * y3, t[n] + dt)
+        Y[:, n + 1] = Y[:, n] + dt * (y1 + 2*y2 + 2*y3 + y4) / 6
 
-    return Y
+    return Y, t
 
